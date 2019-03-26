38736f461e4ae9bc499ea9101c0fbebfe8026f4a
wills <wills.wu@wisdomaic.com>
2019-03-26 14:46:04 -0700

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <phoneLogin />
+  </div>
+</template>
+
+<script>
+import phoneLogin from './components/phoneLogin.vue'
+
+export default {
+  name: 'app',
+  components: {
+    phoneLogin
+  }
+}
+</script>
+
+<style>
+  @import '~flag-icon-css/css/flag-icon.min.css';
+</style>
